1d63720669917f119c95a6ac9332448ae6854717
MusiliC <musilibrian07@gmail.com>
2023-12-11 13:44:38 +0300

    Iplemented post method for all entities

--- src/main/java/com/cee/tech/rest/api/FixturesRestApi.java
+++ src/main/java/com/cee/tech/rest/api/FixturesRestApi.java
@@ -1,13 +1,12 @@
 package com.cee.tech.rest.api;
 
 import com.cee.tech.app.bean.sharedbean.FixtureBeanI;
+import com.cee.tech.app.model.entity.BookTicket;
 import com.cee.tech.app.model.entity.Fixture;
 
 
 import javax.ejb.EJB;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -16,6 +15,17 @@ public class FixturesRestApi extends BaseRestApi{
 
     @EJB(beanName = "userFixtureBean")
     FixtureBeanI fixtureBeanI;
+
+    @Path("/add")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response add(Fixture fixture) {
+
+        fixture = fixtureBeanI.addOrUpdate(fixture);
+        return respond();
+    }
+
     @Path("/list")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
--- src/main/java/com/cee/tech/rest/api/TicketManagementRestApi.java
+++ src/main/java/com/cee/tech/rest/api/TicketManagementRestApi.java
@@ -2,13 +2,12 @@ package com.cee.tech.rest.api;
 
 import com.cee.tech.app.bean.adminbean.AdminTicketManagementI;
 import com.cee.tech.app.bean.sharedbean.FixtureBeanI;
+import com.cee.tech.app.model.entity.BookTicket;
 import com.cee.tech.app.model.entity.Fixture;
 import com.cee.tech.app.model.entity.TicketManagement;
 
 import javax.ejb.EJB;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,6 +16,17 @@ public class TicketManagementRestApi extends BaseRestApi{
 
     @EJB
     AdminTicketManagementI ticketManagementI;
+
+    @Path("/add")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response add(TicketManagement ticketManagement) {
+
+        ticketManagement = ticketManagementI.addOrUpdate(ticketManagement);
+        return respond();
+    }
+
     @Path("/list")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
